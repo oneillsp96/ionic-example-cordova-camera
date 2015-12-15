502597e1584791c59dea0df8839bb7d4badf945f
oneillsp96 <oneillsp96@gmail.com>
2015-12-14 21:00:22 -0500

    camera app working with imageUris, about to try base64 images and probably break stuff

--- www/js/app.js
+++ www/js/app.js
@@ -5,35 +5,67 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.services', 'firebase'])
 
-.config(function($compileProvider){
-  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-})
+  .config(function ($compileProvider) {
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+  })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.controller('MainCtrl', function($scope, Camera) {
-
-  $scope.getPhoto = function() {
-    Camera.getPicture().then(function(imageURI) {
-      console.log(imageURI);
-      $scope.lastPhoto = imageURI;
-    }, function(err) {
-      console.err(err);
-    }, {
-      quality: 75,
-      targetWidth: 320,
-      targetHeight: 320,
-      saveToPhotoAlbum: false
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
     });
-  };
+  })
 
-})
+  .controller('MainCtrl', function ($scope, Camera, $firebaseArray) {
+
+    var ref = new Firebase("https://glaring-heat-7252.firebaseio.com/picList");
+    var picList = $firebaseArray(ref);
+    $scope.picList = picList;
+    
+    $scope.getPhoto = function () {
+      Camera.getPicture().then(function (imageURI) {
+        console.log(imageURI);
+        $scope.lastPhoto = imageURI;
+        picList.$add({ "imageUri": imageURI });
+        $scope.picList = picList;
+      }, function (err) {
+        console.err(err);
+      },  
+        
+        //options
+        {
+          
+          
+          // destinationType: Camera.DestinationType.DATA_URL,//
+          // sourceType: Camera.PictureSourceType.CAMERA,//
+          // allowEdit: true,//
+          // encodingType: Camera.EncodingType.JPEG,//
+          //popoverOptions: CameraPopoverOptions,
+          quality: 75,
+          targetWidth: 320,
+          targetHeight: 320,
+          saveToPhotoAlbum: false
+        });
+    };
+
+  })
+
+//example from cordova camera 
+//Take photo and retrieve Base64-encoded image:
+
+// navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+//     destinationType: Camera.DestinationType.DATA_URL
+//  }); 
+
+// function onSuccess(imageData) {
+//     var image = document.getElementById('myImage');
+//     image.src = "data:image/jpeg;base64," + imageData;
+// }
+
+// function onFail(message) {
+//     alert('Failed because: ' + message);
+// }
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,42 +1,44 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['firebase'])
 
-.config(function($compileProvider){
-  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-})
+  .config(function ($compileProvider) {
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+  })
 
-.controller('DashCtrl', function($scope) {
-})
 
-.controller('FriendsCtrl', function($scope, Friends, Camera) {
-  $scope.friends = Friends.all();
-  $scope.getPhoto = function() {
-    console.log('Getting camera');
-    Camera.getPicture({
-      quality: 75,
-      targetWidth: 320,
-      targetHeight: 320,
-      saveToPhotoAlbum: false
-    }).then(function(imageURI) {
-      console.log(imageURI);
-      $scope.lastPhoto = imageURI;
-    }, function(err) {
-      console.err(err);
-    });
-    /*
-    navigator.camera.getPicture(function(imageURI) {
-      console.log(imageURI);
-    }, function(err) {
-    }, { 
-      quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL
-    });
-    */
-  }
-})
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+  // .controller('FriendsCtrl', function ($scope, Friends, Camera) {
+  //   $scope.friends = Friends.all();
+  //   $scope.getPhoto = function () {
+  //     console.log('Getting camera');
+  //     Camera.getPicture({
+  //       destinationType: Camera.DestinationType.DATA_URL,//
+  //       sourceType: Camera.PictureSourceType.CAMERA,//
+  //       allowEdit: true,//
+  //       encodingType: Camera.EncodingType.JPEG,//
+  //       //popoverOptions: CameraPopoverOptions,
+  //       quality: 75,
+  //       targetWidth: 320,
+  //       targetHeight: 320,
+  //       saveToPhotoAlbum: false
+  //     }).then(function (imageURI) {
+  //       console.log(imageURI);
+  //       $scope.lastPhoto = imageURI;
+  //     }, function (err) {
+  //       console.err(err);
+  //     });
 
-.controller('AccountCtrl', function($scope) {
+
+  //   }
+  // })
+          
+/*
+navigator.camera.getPicture(function(imageURI) {
+  console.log(imageURI);
+}, function(err) {
+}, { 
+  quality: 50,
+  destinationType: Camera.DestinationType.DATA_URL
 });
+*/
+
+
